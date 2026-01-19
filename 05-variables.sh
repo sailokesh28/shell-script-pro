25aec263177ffb481421f1ee1040b8250a969bc7
Sai Lokesh <sailokesheduru95.com>
2026-01-19 23:45:06 +0530

    conservation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,6 @@ echo "Please Enter yoour Username::"  #text entered here will be added as vale t
 read -s USERNAME  #here username is variable name
 
 echo "username entered :: $USERNAME"
+
+echo "Please Enter your Password::"
+read -s PASSWORD  #-s is for silent mode it will not show password on screen
