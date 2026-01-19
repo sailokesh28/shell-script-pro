20e6d0873be051c754b7ec6d5e2bce42408e8439
Sai Lokesh <sailokesheduru95.com>
2026-01-19 23:28:45 +0530

    conservation added

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,8 @@
 #!bin/bash
-echo "Rahim: Hi Robert"
-echo "Robert: Hello Rahim"
-echo "Rahim: How are you?"
-echo "Robert: I am fine how about you?"
+
+PERSON1="Ramesh"
+PERSON2="Suresh"
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you?"
+echo "$PERSON2: I am fine how about you?"
