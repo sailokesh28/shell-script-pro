f58453a94b1a7e4aa79a62844cf4a23918339798
Sai Lokesh <sailokesheduru95.com>
2026-01-20 22:58:03 +0530

    shell script

--- 09-condition.sh
+++ 09-condition.sh
@@ -12,9 +12,9 @@
 
 NUMBER=$1
 
-if [$NUMBER -gt 100]
+if [ $NUMBER -gt 100 ]
 then
- echo "Given Number is Greater than 100"
+    echo "Given Number is Greater than 100"
 else 
     echo "Given Number is Less than or Equal to 100"
-    fi
+fi
