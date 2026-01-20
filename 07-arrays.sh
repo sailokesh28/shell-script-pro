a3074fa3e655b7df2553890856b1ac019559225c
Sai Lokesh <sailokesheduru95.com>
2026-01-20 22:24:01 +0530

    shell script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,8 +1,7 @@
 #!bin/bash
 
 FRUITS=("Apple" "Banana" "Cherry")
-
-echo "Fruits in the array:${FRUITS[@]}"
+echo "first fruit:${FRUITS[0]}"
 echo "second fruit:${FRUITS[1]}"
 echo "third fruit:${FRUITS[2]}"
 echo "Total number of fruits:${#FRUITS[@]}"
