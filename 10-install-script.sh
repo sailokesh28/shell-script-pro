a8e1903375267878b7007c095625ac0bf10b9dcd
Sai Lokesh <sailokesheduru95.com>
2026-01-20 23:20:39 +0530

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -6,5 +6,21 @@ USERID=$(id -u)
         echo "You must run this script as root user"
         exit 1
     fi
+
         dnf install mysql -y
+        if [ $? -ne 0 ]
+        then 
+        echo "MySQL installation failed"
+        exit 1
+        else
+        echo "MySQL installed successfully"
+        fi
+
         dnf install git -y
+    if [ $? -ne 0 ]
+        then 
+        echo "git installation failed"
+        exit 1
+        else
+        echo "git installed successfully"
+        fi
