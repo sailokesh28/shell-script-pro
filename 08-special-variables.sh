f235ecd35cfbe86c89034c6265245a87cdba559c
Sai Lokesh <sailokesheduru95.com>
2026-01-20 22:37:35 +0530

    shell script

---
+++ 08-special-variables.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+echo "All Variables is passed : $@"
+echo "Total Number of Variables : $#"
+echo "script Name : $0"
+echo "present working directory : $PWD"
+echo  "home directory : $HOME"
+echo "which user is executing the script : $USER
+echo "process id of the current script : $$"
+echo "last command exit status : $?"
