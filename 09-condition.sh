1b0fe955d07f25c79b0da6096cea3cb758302f5d
Sai Lokesh <sailokesheduru95.com>
2026-01-20 22:54:31 +0530

    shell script

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,11 +1,20 @@
 #!bin/bash
 
-Today=$(date +%Y-%m-%d)
-HOUR=$(date +%H)
-if [ $HOUR -lt 12 ]; then
-    echo "Good Morning! Today is $Today"
-elif [ $HOUR -lt 22 ]; then
-    echo "Good Afternoon! Today is $Today"
-else
-    echo "Good Evening! Today is $Today"
-fi
+# Today=$(date +%Y-%m-%d)
+# HOUR=$(date +%H)
+# if [ $HOUR -lt 12 ]; then
+#     echo "Good Morning! Today is $Today"
+# elif [ $HOUR -lt 22 ]; then
+#     echo "Good Afternoon! Today is $Today"
+# else
+#     echo "Good Evening! Today is $Today"
+# fi
+
+NUMBER=$1
+
+if [$Number -gt 100]
+then
+ echo "Given Number is Greater than 100"
+else 
+    echo "Given Number is Less than or Equal to 100"
+    fi
